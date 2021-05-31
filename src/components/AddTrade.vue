34f87a96e3351132acbdb2a805fe700860a13acc
felix <felixHuang0619@gmail.com>
2021-05-31 11:39:25 +0800

    feat: add trade api

--- src/components/AddTrade.vue
+++ src/components/AddTrade.vue
@@ -6,11 +6,33 @@
     label-width="80px"
   >
     <el-form-item label="日期" label-width="50px">
-      <el-date-picker v-model="tradeInfo.date" type="date" placeholder="選擇日期">
-      </el-date-picker>
+      <el-col :span="12">
+        <el-date-picker
+          v-model="tradeInfo.date"
+          type="date"
+          format="YYYY-MM-DD"
+          value-format="YYYY-MM-DD"
+          placeholder="選擇日期"
+        >
+        </el-date-picker>
+      </el-col>
+    </el-form-item>
+    <el-form-item label="股號" label-width="50px">
+      <el-select v-model="tradeInfo.stock_id" filterable placeholder="請選擇">
+        <el-option
+          v-for="oneStock in stockList"
+          :key="oneStock.stock_id"
+          :label="oneStock.stock_id + ' ' + oneStock.stock_name"
+          :value="oneStock.stock_id"
+        >
+        </el-option>
+      </el-select>
     </el-form-item>
     <el-form-item label="股數" label-width="50px">
-      <el-input placeholder="買入股數" v-model="tradeInfo.num" clearable> </el-input>
+      <el-col :span="10">
+        <el-input v-model="tradeInfo.num" clearable>
+        </el-input>
+      </el-col>
     </el-form-item>
     <el-form-item label="成交價" label-width="70px">
       <el-input-number
@@ -20,35 +42,50 @@
         :max="1000"
       ></el-input-number>
     </el-form-item>
-    <el-form-item> 
-        <el-button type="success" icon="el-icon-check" circle></el-button>
+    <el-form-item>
+      <el-button
+        type="success"
+        icon="el-icon-check"
+        circle
+        @click="onSubmit"
+      ></el-button>
     </el-form-item>
   </el-form>
 </template>
 
 <script>
-import { db } from '../firebase.js';
+import { db } from "../firebase.js";
 
 export default {
   data() {
     return {
-        tradeInfo: {
-            date: "",
-            num: 1,
-            tradePrice: 0,
-        },
-        testData: []
+      tradeInfo: {
+        date: "",
+        num: 1,
+        tradePrice: 0,
+        stock_id: "",
+      },
+      testData: [],
+      stockList: [],
     };
   },
   firestore() {
-      return {
-          testData: db.collection('trade')
-      }
+    return {
+      testData: db.collection("trade"),
+      stockList: db.collection("stock_list"),
+    };
   },
   methods: {
-    //   post() {
-          
-    //   }
-  }
+    onSubmit() {
+      db.collection("trade").add({
+        stock_id: this.tradeInfo.stock_id,
+        is_sell_out: true,
+        num: this.tradeInfo.num,
+        trade_price: this.tradeInfo.tradePrice,
+        date: "2021-05-06",
+      });
+      console.log("submit", this.tradeInfo);
+    },
+  },
 };
 </script>
--- src/components/TradingList.vue
+++ src/components/TradingList.vue
@@ -1,47 +1,79 @@
 <template>
   <el-table
-    :data="tableData"
+    :data="getTradeLog()"
     style="width: 100%"
     :row-style="tableRowStyle"
     :header-cell-style="tableHeaderColor"
   >
     <el-table-column prop="date" label="日期" width="180"> </el-table-column>
-    <el-table-column prop="action" label="操作" width="80"> </el-table-column>
-    <el-table-column prop="stockNo" label="編號" width="150"> </el-table-column>
-    <el-table-column prop="tradePrice" label="成交價" width="100"> </el-table-column>
-    <el-table-column prop="num" label="股數"> </el-table-column>
-    <el-table-column prop="cost" label="成本"> </el-table-column>
-    <el-table-column prop="income" label="實現損益 Income(Loss)" width="300"> </el-table-column>
+    <el-table-column label="操作" width="80">
+      <template #default="scope">
+        <span v-if="scope.row.action == 'buy'">買入</span>
+        <span v-else>賣出</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="stock_no" label="編號" width="150"> </el-table-column>
+    <el-table-column prop="trade_price" label="成交價" width="100">
+    </el-table-column>
+    <el-table-column label="股數">
+      <template #default="scope">
+        <span>{{ scope.row.num }}</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="cost" label="成本">
+      <template #default="scope">
+        <span v-if="scope.row.action == 'buy'">{{ scope.row.fee }}</span>
+        <span v-else>0</span>
+      </template>
+    </el-table-column>
+    <el-table-column prop="income" label="實現損益 Income(Loss)" width="300">
+      <template #default="scope">
+        <span v-if="scope.row.action == 'sale'">{{ scope.row.fee }}</span>
+        <span v-else>0</span>
+      </template>
+    </el-table-column>
   </el-table>
+  <el-pagination background layout="prev, pager, next" :total="1000">
+  </el-pagination>
 </template>
 
 <script>
+import { db } from "../firebase.js";
 export default {
   data() {
     return {
+      tradeLog: [],
       tableData: [
         {
-            date: "2021/05/05",
-            action: "現買",
-            stockNo: "2609 長榮",
-            tradePrice: "84.4",
-            num: "250",
-            cost: "21100",
-            income: "0",
+          date: "2021/05/05",
+          action: "現買",
+          stockNo: "2609 長榮",
+          tradePrice: "84.4",
+          num: "250",
+          cost: "21100",
+          income: "0",
         },
         {
-            date: "2021/05/05",
-            action: "現買",
-            stockNo: "2609 長榮",
-            tradePrice: "84.4",
-            num: "250",
-            cost: "21100",
-            income: "0",
-        }
+          date: "2021/05/05",
+          action: "現買",
+          stockNo: "2609 長榮",
+          tradePrice: "84.4",
+          num: "250",
+          cost: "21100",
+          income: "0",
+        },
       ],
     };
   },
+  firestore() {
+    return {
+      tradeLog: db.collection("trade_log").orderBy('date', 'desc'),
+    };
+  },
   methods: {
+    getTradeLog: function () {
+      return this.tradeLog;
+    },
     tableRowStyle({ row, rowIndex }) {
       return "background-color: rgb(48, 44, 44);color: #fff;";
     },
