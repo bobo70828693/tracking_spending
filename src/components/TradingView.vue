93c07a9000883b9691c33ce1767c3791b24fd64f
felix <felixHuang0619@gmail.com>
2021-06-08 22:24:50 +0800

    feat: add fugle api

--- src/components/TradingView.vue
+++ src/components/TradingView.vue
@@ -1,17 +1,17 @@
 <template>
   <el-table
-    :data="getTrade()"
+    :data="tradeData"
     style="width: 100%"
     :row-style="tableRowStyle"
     :header-cell-style="tableHeaderColor"
   >
     <el-table-column prop="stockID" label="股名" width="180"> </el-table-column>
-    <el-table-column prop="tradePrice" label="現價" width="150">
+    <el-table-column prop="priceReference" label="現價" width="150">
     </el-table-column>
     <el-table-column prop="tradePrice" label="成交均價" width="100">
     </el-table-column>
     <el-table-column prop="num" label="股數"> </el-table-column>
-    <el-table-column prop="income" label="未實現損益 Income(Loss)">
+    <el-table-column prop="income" label="未實現損益 Income(Loss)" width="300">
     </el-table-column>
     <!-- <template #default="scope">
         <span v-if="scope.row.action == 'sale'">{{ scope.row.fee }}</span>
@@ -30,7 +30,9 @@
           >
         </div>
         <template #reference>
-          <el-button type="warning" id="soldOut" @click="visible = true">平倉</el-button>
+          <el-button type="warning" id="soldOut" @click="visible = true"
+            >平倉</el-button
+          >
         </template>
       </el-popover>
     </el-table-column>
@@ -39,38 +41,39 @@
 
 <script>
 import { db } from "../firebase.js";
+import axios from "axios";
+
 export default {
   data() {
     return {
       tradeView: [],
-      tradeData: [],
+      tradeData: this.handleTradeData(),
+      stockInfo: []
     };
   },
-  // firestore() {
-  //   return {
-  //     tradeView: db.collection("trade").orderBy("date", "desc"),
-  //   };
-  // },
-  mounted() {
+  created() {
     db.collection("trade")
       .orderBy("date", "desc")
       .get()
       .then((currentValue) => {
         this.tradeView = currentValue.docs.map((doc) => doc.data());
       });
-    // this.fetchAPI()
   },
-  computed: {
-    handleTradeData: function () {
+  methods: {
+    async handleTradeData() {
       let stockIDList = new Set(...[this.tradeView.map((val) => val.stock_id)]);
+      let stockIDListAr = Array.from(stockIDList);
       let tradeData = [];
-      stockIDList.forEach((val) => {
+
+      for(let i = 0; i < stockIDListAr.length; i++) {
         let data = [];
+        await this.getStockInfo(stockIDListAr[i]);
         this.tradeView.forEach((currentValue) => {
-          if (val == currentValue.stock_id) {
+          if (stockIDListAr[i] == currentValue.stock_id) {
             if (data.length == 0) {
               data = {
                 stockID: currentValue.stock_id,
+                priceReference: this.stockInfo.priceReference,
                 isSellOut: currentValue.is_sell_out,
                 num: parseInt(currentValue.num),
                 tradePrice: currentValue.trade_price,
@@ -78,6 +81,7 @@ export default {
             } else {
               data = {
                 stockID: currentValue.stock_id,
+                priceReference: this.stockInfo.priceReference,
                 isSellOut: currentValue.is_sell_out,
                 num: parseInt(data.num) + parseInt(currentValue.num),
                 tradePrice:
@@ -89,14 +93,31 @@ export default {
           }
         });
         tradeData.push(data);
-      });
-
-      return (this.tradeData = tradeData);
+      }
+      console.log(tradeData);
+      return tradeData;
     },
-  },
-  methods: {
     getTrade: function () {
       return this.tradeData;
+    },
+    getStockInfo(stockID) {
+      console.log("stockID ===> ", stockID);
+      let url = "https://api.fugle.tw/realtime/v0.2/intraday/meta?symbolId="+ stockID +"&apiToken=";
+
+      const headers = {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      };
+
+      let stockInfo = axios.get(url, { headers })
+        .then((res) => {
+          this.stockInfo = res.data.data.meta;
+        })
+        .catch(function (error) {
+          console.log("連線異常");
+        });
+
+        return stockInfo;
     },
     tableRowStyle({ row, rowIndex }) {
       return "background-color: rgb(48, 44, 44);color: #fff;";
